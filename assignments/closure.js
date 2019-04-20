15550a6d1091a6796497df2451b7a1cc9108c9fb
Melvin Peepers <peepersm@gmail.com>
2019-04-20 13:57:52 -0400

    completed the first challenge of Closures

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,13 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+let age = 5;
+function yearsTillSheCanDrive() {
+  let ageDrive = 16;
+  return (ageDrive - age);
+};
+console.log(yearsTillSheCanDrive());
+
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
